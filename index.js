97099c673e7eb689a4d989b69eaffff22085c4b7
iWuzhi <Kaer_600776@163.com>
2019-12-28 09:49:52 +0800

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+* author iWuzhi
+* date 2019-12-28 09:36:06
+*
+*/
+
+// 手机号码
+export const MOBILE_PHONE = /^1[3456789]\d{9}$/;
+
+// 正整数
+export const POSITION_INTEGER = /^[1-9]\d*$/;
+
+// 负整数
+export const NEGATIVE_INTEGER = /^-[1-9]\d*$/;
+
+// 整数
+export const ALL_INTEGER = /^-?[1-9]\d*$/;
+
+
+
+
+
